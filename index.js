3db82baffbfcb0b1b343832f280a559916e1cf77
Abdul Rehman <sheepc56@gmail.com>
2023-12-31 12:10:11 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function previous() {
+    var image = document.getElementById(img1).scr= `Image_created_with_a_mobile_phone.png`;
+}
+function next() {
+    var image = document.getElementById(img2).scr= `istockphoto-1322277517-612x612.jpg`;
+}
+
